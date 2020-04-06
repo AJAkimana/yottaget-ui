b19c2b93387af5b46f80f8622612358c8df5f88e
Akimana Linux <akimana@linux.me>
2020-04-06 10:50:03 +0200

    Navbar for react-bootstrap

--- src/views/homepage.js
+++ src/views/homepage.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Container, Divider } from '@material-ui/core';
 
 const Homepage = () => {
   return (
-    <Navbar expand='lg' variant='light' bg='light'>
-      <Container>
-        <Navbar.Brand href='#'>Navbar</Navbar.Brand>
-      </Container>
+    <Navbar bg='primary' variant='dark'>
+      <Navbar.Brand href='#home'>Yottaget</Navbar.Brand>
+      <Nav className='mr-auto'></Nav>
+      <Nav className='mr-auto'>
+        <Nav.Link href='#home'>Home</Nav.Link>
+        <Nav.Link href='#features'>Add your house</Nav.Link>
+        <Nav.Link href='#pricing'>Help</Nav.Link>
+        <Nav.Link href='#pricing'>Sign up</Nav.Link>
+        <Nav.Link href='#pricing'>Log in</Nav.Link>
+      </Nav>
+      <Nav>
+        <Button variant='danger'>Contact us!</Button>
+      </Nav>
     </Navbar>
   );
 };
